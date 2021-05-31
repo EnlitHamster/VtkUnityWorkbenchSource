a7fd8589121c0f0db3d2934204233f72d3a5555f
EnlitHamster <san.massa.98@gmail.com>
2021-05-31 12:32:19 +0200

    Auto stash before merge of "dev" and "vuw/main"

---
+++ VtkToUnityExample/Assets/PipelineBlockButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PipelineBlockButton : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Image>().color = Color.gray;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        GetComponent<Image>().color = Color.white;
+    }
+}
